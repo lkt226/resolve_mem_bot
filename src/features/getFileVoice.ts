72ea5ab05d1f2f840bf54354b53fe731cedaf9a5
Victor Hugo <83098581+lkt226@users.noreply.github.com>
2024-03-16 18:28:08 -0300

    inicio da funcionalidade de baixar a voz do usuário

---
+++ src/features/getFileVoice.ts
@@ -0,0 +1,22 @@
+import bot from "../config";
+
+import { Voice } from "grammy/types";
+import consumeApi from "../utils/api";
+
+const getFileVoiceUrl = async (voice: Voice) => {
+    const file = await bot.api.getFile(voice.file_id)
+    if (!file.file_path) {
+        console.error("No file path found.")
+        return
+    } 
+
+    const url = consumeApi.getFile(file.file_path)
+    return url
+}
+
+const getFileVoice = async (voice: Voice) => {
+  const url = getFileVoiceUrl(voice)
+  console.log(url)
+}
+
+export default getFileVoice;
