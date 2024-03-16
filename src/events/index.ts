964717f49b20afb35ed572c97f5ad48990ced560
Victor Hugo <83098581+lkt226@users.noreply.github.com>
2024-03-16 17:52:07 -0300

    Download de audio gravado no Telegram

--- src/events/index.ts
+++ src/events/index.ts
@@ -1,9 +1,25 @@
 import bot from "../config";
 
+import { Message, Voice } from "grammy/types";
+import consumeApi from "../utils/api";
 
-const execute =  (screaming:boolean) => {
+const getFileVoice = async (voice: Voice) => {
+    const file = await bot.api.getFile(voice.file_id)
+    if (!file.file_path) {
+        console.error("No file path found.")
+        return
+    } 
+
+    const link = consumeApi.getFile(file.file_path)
+    return link
+}
+
+const execute =  (_:boolean) => {
     bot.on("message", async (ctx) => {
-        
+        if (ctx.message?.voice) {
+            const link = await getFileVoice(ctx.message.voice)
+            console.log(link)
+        }
     });
 }
 
