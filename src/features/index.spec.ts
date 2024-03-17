a9e23fe3186081733adad585da6a8b337feedeb5
Victor Hugo <83098581+lkt226@users.noreply.github.com>
2024-03-17 00:20:08 -0300

    inicio do TDD baseado nas funcionalidades

---
+++ src/features/index.spec.ts
@@ -0,0 +1,20 @@
+// Esse documento deve fazer a conexão entre as outras funcionalidades do projeto
+
+import getRandomMessage from './getRandomMessage'
+
+describe('Integrate all functions', () => {
+  it ('select random message in data -->  random textf in data', () => {})
+  it ('send message to user --> user receive one message', () => {})
+  it ('save message in database --> new message in database', () => {})
+  it ('delete message after 1 second --> message deleted', () => {})
+  it ('send a message to user in 2 second --> user receive one message', () => {})
+  
+  it ('alert to user if he not send a message in 1 second --> user receive one message', () => {})
+  
+  it ('receive a voice message by user --> has one .OGG archive in storage', () => {})
+  it ('transcription of voice message --> has one transcription in database', () => {})
+  it ('clear the storage --> storage is empty', () => {})
+  it ('validate the transcription --> transcription is correct', () => {})
+
+  it ('create a report --> has one report in database', () => {})
+})
