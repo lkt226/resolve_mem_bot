ac7c6fa12d76db3842427379d263511b619762b7
Victor Hugo <83098581+victorhdsp@users.noreply.github.com>
2024-03-20 20:27:54 -0300

    rotina inicial e comandos

--- src/index.ts
+++ src/index.ts
@@ -4,22 +4,13 @@ dotenv.config();
 import "./database";
 
 import bot from "./config";
-import createEvents from "./services";
-import sendMessage from "./features - (rascunho)/sendMessage";
 
-let screaming = false;
+import Routines from "./services/index"
 
-bot.command("scream", () => {
-  screaming = true;
-});
-
-bot.command("whisper", () => {
-  screaming = false;
-});
-
-createEvents(screaming)
-
-sendMessage(process.env.CHAT_ID_TEST||'', 'Bot is running!!')
 
 //Start the Bot
-bot.start();
+(async () => {
+  console.log("Bot iniciado")
+  bot.start()
+  Routines()
+})()
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,20 +1,60 @@
-import bot from "../config";
 import prisma from "../database";
-import Collector from "./collector";
+import bot from "../config";
+
 import Identifier from "./identifier";
+import Questioner from "./questioner";
+import Collector from "./collector";
+import Receiver from "./receiver";
 
 const identifier = new Identifier(prisma)
 const collector = new Collector(prisma)
+const receiver = new Receiver(prisma)
 
-const execute = (_: boolean) => {
-  bot.on("message", async (ctx) => {
-    if (ctx.message?.voice) {
-      await collector.handleAudioFromUser(ctx)
+const messageBank = [`Vivo assim,
+entre tropeços e ganhos.
+Vivo em um mundo só meu,
+amo canções, versos e rimas.
+Perco-me em meus devaneios.`]
 
-    } else if (ctx.message?.text?.includes("/")){
-      await identifier.handleIncomingMessage(ctx)
-    }
-  });
+export const routineForFirstMessageInDay = async () => {
+  const users = await prisma.chat.findMany()
+  users.forEach(async (user) => {
+    const questioner = new Questioner(messageBank, prisma, parseInt(user.chat_id))
+    await questioner.handleSendFirstMessage()
+  })
 }
 
-export default execute;
+export const routineForRecurrentMessage = async () => {
+  const users = await prisma.chat.findMany()
+
+  users.forEach(async (user) => {
+    const questioner = new Questioner(messageBank, prisma, parseInt(user.chat_id))
+    await questioner.handleSendRecorrentMessage()
+  })
+}
+
+const execute = () => {
+  bot.on("message", async (context) => {
+    const voice = context.message?.voice
+    const chatId = context.chat?.id
+
+    if (voice && chatId) {
+      const user = await prisma.chat.findFirst({ where: { chat_id: `${chatId}` } })
+  
+      if (!user) { throw new Error("User not found")}
+      const voiceMessage = await collector.handleAudioFromUser(context)
+      if (!voiceMessage) { throw new Error("Voice message not found")}
+      await receiver.handleAudioTranscription(voiceMessage.id)
+    }
+
+    if (context.message?.text?.includes("/")){
+      await identifier.handleIncomingMessage(context)
+    }
+  })
+  
+  setTimeout(routineForFirstMessageInDay, 1000)
+  
+  // setInterval(routineForRecurrentMessage, 1000 * 30) // 30 seconds
+}
+
+export default execute
