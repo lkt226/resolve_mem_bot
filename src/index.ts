e38889378678aa6b6b4fb5f57e95b4702b383ca4
Victor Hugo <83098581+victorhdsp@users.noreply.github.com>
2024-03-22 11:56:33 -0300

    deploy with webhook

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,27 @@
 import dotenv from "dotenv";
 dotenv.config();
+import express from 'express'
 
 import "./database";
 
 import bot from "./config";
 
 import Routines from "./services/index"
+import { webhookCallback } from "grammy";
 
+if (process.env.NODE_ENV === "production") {
+  const app = express();
+  app.use(express.json());
+  app.use(webhookCallback(bot, "express"));
 
-//Start the Bot
-(async () => {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    Routines()
+    console.log(`Bot iniciado na porta ${PORT}`);
+  });
+} else {
   bot.start({ onStart: () => {
     Routines()
     console.log("Bot iniciado")
-  }, drop_pending_updates: true })
-})
+  }})
+}
