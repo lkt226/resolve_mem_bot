cec69172f9778f3ef5f6ef73d89adab521877114
Victor Hugo <83098581+victorhdsp@users.noreply.github.com>
2024-03-22 12:14:53 -0300

    cors e mensagem inicial

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 import express from 'express'
+import cors from 'cors'
 
 import "./database";
 
@@ -11,14 +12,24 @@ import { webhookCallback } from "grammy";
 
 if (process.env.NODE_ENV === "production") {
   const app = express();
+
+  app.use(cors())
   app.use(express.json());
   app.use(webhookCallback(bot, "express"));
+  
+  app.get("/", (req, res) => {
+    res.json({
+      status: "ok",
+      message: "Bot iniciado"
+    });
+  })
 
   const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
     Routines()
     console.log(`Bot iniciado na porta ${PORT}`);
   });
+
 } else {
   bot.start({ onStart: () => {
     Routines()
